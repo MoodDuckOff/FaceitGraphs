4661cf255803573f5a063c8945c28156b4380063
Vitaliy Shatskiy <kaptowkabest@gmail.com>
2021-01-12 07:15:15 +0200

    added request exception handler

---
+++ FaceitGraphs-backend/FaceitGraphs-backend/Helpers/Exceptions/RequestException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FaceitGraphs_backend.Helpers.Exceptions
+{
+    public class RequestException : Exception
+    {
+        public int HttpStatusCode { get;}
+
+        public RequestException(string message, int httpStatusCode) : base(message)
+        {
+            HttpStatusCode = httpStatusCode;
+        }
+    }
+}
